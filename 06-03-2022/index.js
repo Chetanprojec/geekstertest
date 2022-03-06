5cb33e1c042db74edb497fb344fa0e12db30ece1
Chetanprojec <96860008+Chetanprojec@users.noreply.github.com>
2022-03-06 15:41:48 +0530

    Add files via upload

---
+++ 06-03-2022/index.js
@@ -0,0 +1,20 @@
+function samclick(){
+    var a = document.getElementById("sam")
+    a.classList.add("one")
+   
+    
+}
+
+function samclick1(){
+    var a = document.getElementById("viv")
+    a.classList.add("two")
+   
+    
+}
+
+function samclick2(){
+    var a = document.getElementById("opp")
+    a.classList.add("three")
+   
+    
+}
